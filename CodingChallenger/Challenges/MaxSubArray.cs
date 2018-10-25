b26f0aa65613b7eb48cf4760362573f7cfb7a10e
Ethan Tang <eytang@uwaterloo.ca>
2018-10-25 03:35:13 -0400

    Added MaxSubArray

---
+++ CodingChallenger/Challenges/MaxSubArray.cs
@@ -0,0 +1,42 @@
+﻿using CodingChallenger.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenger.Challenges {
+    /// <summary>
+    /// Kadane's, read it
+    /// </summary>
+    [Challenge(Challenge.NotDone)]
+    class MaxSubArray : IChallenge<int[], int> {
+        public int ExpectedOutput() {
+            return 6;
+        }
+
+        public int[] Input() {
+            return new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        }
+
+        public int Run(int[] input) {
+            var nums = input;
+
+            var bestMax = 0;
+            var currentMax = 0;
+
+            foreach (var num in nums) {
+                currentMax = Math.Max(currentMax + num, num);
+                if (bestMax < currentMax) {
+                    bestMax = currentMax;
+                }
+            }
+
+            if (bestMax == 0) {
+                bestMax = nums.Max();
+            }
+
+            return bestMax;
+        }
+    }
+}
