e891cd7adf6f05f2e787ed7463e2ec88fd40f0b7
Ethan Tang <eytang@uwaterloo.ca>
2018-11-21 14:34:12 -0500

    Added jump game

---
+++ CodingChallenger/Challenges/JumpGame.cs
@@ -0,0 +1,47 @@
+﻿using CodingChallenger.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenger.Challenges {
+    /// <summary>
+    /// Greedy, take the jump if it is higher... also watch out for end conditions
+    /// </summary>
+    [Challenge(Challenge.Done)]
+    class JumpGame : IChallenge<int[], bool> {
+        public bool ExpectedOutput() {
+            return false;
+        }
+
+        public int[] Input() {
+            return new int[5] { 3, 2, 1, 0, 4 };
+        }
+
+        public bool Run(int[] input) {
+            var nums = input;
+
+            if (nums.Length <= 1) {
+                return true;
+            }
+
+            var idx = 0;
+            var jumps = nums[idx];
+
+            while (jumps > 0 || nums[idx] > 0) {
+                if (nums[idx] > jumps) {
+                    jumps = nums[idx];
+                } else {
+                    jumps--;
+                    idx++;
+                }
+                if (idx == nums.Length - 1) {
+                    break;
+                }
+            }
+
+            return idx == nums.Length - 1;
+        }
+    }
+}
